31e85a0afe19fa14ad935f8f94206e6c8ed1051e
AbygaelleJoly <2444950@cegepba.qc.ca>
2025-02-11 11:41:19 -0500

    Creation de la classe Personnage

---
+++ PO1_Encapsulation/Personnage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO1_Encapsulation
+{
+    public class Personnage
+    {
+        int pointDeVie;
+        int attaque;
+
+        public int PointDeVie { get => pointDeVie; set { if (value<0)
+                {value=0 
+                { 
+
+} } =>  pointDeVie = value; }
+        public int Attaque { get => attaque; set => attaque = value; }
+    }
+}
